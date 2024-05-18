8b63e8b4f7c915ddd685b900006ce8e761f4cdec
NeverLand <zhitao.xu@mail.utoronto.ca>
2024-05-18 14:50:55 -0400

    p21 in progress

---
+++ cpp/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+   public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // Handle edge cases
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+
+        // Now we know that both lists are not empty
+        // need to return a new ListNode that contains the merged list
+
+        ListNode* mergedList = new ListNode();
+        // TODO: check the syntax
+
+    }
+};
+// @lc code=end
