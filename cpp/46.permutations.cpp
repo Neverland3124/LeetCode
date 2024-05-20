632b4213ba7cac9e5649e504fec728e9c9a05308
NeverLand <zhitao.xu@mail.utoronto.ca>
2024-05-20 17:33:33 -0400

    permutation finish?

--- cpp/46.permutations.cpp
+++ cpp/46.permutations.cpp
@@ -10,49 +10,86 @@
 #include <vector>
 using namespace std;
 
+// class Solution {
+//    public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> result;
+//         backtrack(nums, result, 0);
+//         return result;
+//     }
+
+//     // when index == nums.size() - 1, backtrack will add the current nums to
+//     // result
+//     // when index < nums.size() - 1, we will be swapping index with all
+//     // elements after index and eventually add nums to result
+//     void backtrack(vector<int>& nums, vector<vector<int>>& result, int index) {
+//         if (index == nums.size() - 1) {
+//             result.push_back(nums);
+//             return;
+//         }
+
+//         assert(index < nums.size());
+
+//         // the following loop will swap index with all elements that after
+//         // index, then recursively call backtrack with index + 1
+//         // by doing so,
+//         // [1, index, 3, 4] will have index swap with 3, 4
+//         // [1, 3, index, 4] with new index at position 3
+//         // this will go over the original 1324 and swapped 1342
+//         // [1, 3, 4, index] with new index at position 4
+//         // so generally this will work because the new backtrack will return the
+//         // permutation of rest of nums for loop will return the permutation of
+//         // current index of the nums
+//         for (int i = index; i < nums.size(); i++) {
+//             // swap(x, y) will swap x and y in the array nums
+//             swap(nums[index], nums[i]);
+//             backtrack(nums, result, index + 1);
+//             swap(nums[index], nums[i]);
+//         }
+//         // nums = [1,2,3], index = 0
+//         // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,2,1],[3,1,2]]
+//         // first index then i
+//         // a00 a11 a22 r22 [1,2,3] a12 r12 [1,3,2] r11
+//         // a01 a11 a22 r22 [2,1,3] a12 r12 [2,3,1] r11
+//         // a02 a11 a22 r22 [3,2,1] a12 r12 [3,1,2] r11
+//         // end
+//     }
+// };
+
 class Solution {
    public:
     vector<vector<int>> permute(vector<int>& nums) {
+        // Second Idea: Use currentPermutation array instead of index
         vector<vector<int>> result;
-        backtrack(nums, result, 0);
+        vector<bool> used(nums.size(), false);
+        vector<int> currentPermutation;
+        // it starts with an empty currentPermutation
+
+        sort(nums.begin(), nums.end());
+        backtrack(nums, result, currentPermutation, used);
+
         return result;
     }
 
-    // when index == nums.size() - 1, backtrack will add the current nums to
-    // result
-    // when index < nums.size() - 1, we will be swapping index with all
-    // elements after index and eventually add nums to result
-    void backtrack(vector<int>& nums, vector<vector<int>>& result, int index) {
-        if (index == nums.size() - 1) {
-            result.push_back(nums);
+   private:
+    void backtrack(vector<int>& nums, vector<vector<int>>& result, vector<int>& currentPermutation,
+                   vector<bool>& used) {
+        if (currentPermutation.size() == nums.size()) {
+            result.push_back(currentPermutation);
             return;
         }
 
-        assert(index < nums.size());
-
-        // the following loop will swap index with all elements that after
-        // index, then recursively call backtrack with index + 1
-        // by doing so,
-        // [1, index, 3, 4] will have index swap with 3, 4
-        // [1, 3, index, 4] with new index at position 3
-        // this will go over the original 1324 and swapped 1342
-        // [1, 3, 4, index] with new index at position 4
-        // so generally this will work because the new backtrack will return the
-        // permutation of rest of nums for loop will return the permutation of
-        // current index of the nums
-        for (int i = index; i < nums.size(); i++) {
-            // swap(x, y) will swap x and y in the array nums
-            swap(nums[index], nums[i]);
-            backtrack(nums, result, index + 1);
-            swap(nums[index], nums[i]);
+        for (int i = 0; i < nums.size(); ++i) {
+            // for each i, we will check if it is used, if it is used, we will skip it
+            // here it means the i-th position element is already in the Permutation
+            // this prevents from [1,1,1] cases
+            if (used[i]) continue;
+            used[i] = true;
+            currentPermutation.push_back(nums[i]);
+            backtrack(nums, result, currentPermutation, used);
+            used[i] = false;
+            currentPermutation.pop_back();
         }
-        // nums = [1,2,3], index = 0
-        // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,2,1],[3,1,2]]
-        // first index then i
-        // a00 a11 a22 r22 [1,2,3] a12 r12[1,3,2] r11
-        // a01 a11 a22 r22 [2,1,3] a12 r12[2,3,1] r11
-        // a02 a11 a22 r22 [3,2,1] a12 r12[3,1,2] r11
-        // end
     }
 };
 // @lc code=end
--- cpp/47.permutations-ii.cpp
+++ cpp/47.permutations-ii.cpp
@@ -6,6 +6,8 @@
 
 // @lc code=start
 #include <algorithm>
+#include <iostream>
+#include <set>
 #include <vector>
 using namespace std;
 
@@ -37,28 +39,58 @@ using namespace std;
 //     }
 // };
 
+#include <algorithm>
+#include <vector>
+using namespace std;
+
 class Solution {
    public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        // Second Idea: Use a totally different way dell with swap and remove duplicate is very hard
+        // to implement now we choose now to use swap but just add item to the vector
         vector<vector<int>> result;
-        // Second Idea: remove duplicate before we have the final result, 
-        // which might be faster
+        vector<bool> used(nums.size(), false);
+        vector<int> currentPermutation;
+
         sort(nums.begin(), nums.end());
-        backtrack(nums, result, 0);
+        // don't use index anymore, just currentPermutation array
+        backtrack(nums, result, currentPermutation, used);
+
         return result;
     }
 
-    void backtrack(vector<int>& nums, vector<vector<int>>& result, int index) {
-        if (index == nums.size() - 1) {
-            result.push_back(nums);
+   private:
+    void backtrack(vector<int>& nums, vector<vector<int>>& result, vector<int>& currentPermutation,
+                   vector<bool>& used) {
+        // base case to push to result
+        if (currentPermutation.size() == nums.size()) {
+            result.push_back(currentPermutation);
             return;
         }
 
-        for (int i = index; i < nums.size(); i++) {
-            swap(nums[index], nums[i]);
-            backtrack(nums, result, index + 1);
-            swap(nums[index], nums[i]);
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+
+            // below is the only line change to prevent duplicate
+            // what we want to prevent here is that position 0 and 1 have the same element, then
+            // [p0, p1, p2] will be same as [p1, p0, p2] since both are [1, 1, 2]
+
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                // nums are sorted, and i is not used by previous condition
+                // i > 0 because 0 will not be duplicate with others
+                // in the for loop, it might happens that we previous i-1 is set back to false and
+                // then we use the current i, which is exactly the case of [p1, p0, p2] which we
+                // should avoid, so use this continue to avoid this
+                continue;
+            }
+
+            used[i] = true;
+            currentPermutation.push_back(nums[i]);
+            backtrack(nums, result, currentPermutation, used);
+            used[i] = false;
+            currentPermutation.pop_back();
         }
     }
 };
+
 // @lc code=end
