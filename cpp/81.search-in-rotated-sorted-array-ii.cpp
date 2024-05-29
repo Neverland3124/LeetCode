d6b837c385b3024eb08947e1f3cceb4b76594469
NeverLand <zhitao.xu@mail.utoronto.ca>
2024-05-28 22:26:46 -0400

    working on 81

--- cpp/81.search-in-rotated-sorted-array-ii.cpp
+++ cpp/81.search-in-rotated-sorted-array-ii.cpp
@@ -10,10 +10,45 @@
 using namespace std;
 
 class Solution {
-public:
+   public:
     bool search(vector<int>& nums, int target) {
-        
+        // Question: why the distinct version not working for the non-distinct version?
+        // [1,0,1,1,1] try to find 0, expected: true, output: false with original method
+        // Note:
+        int left = 0;
+        int right = nums.size() - 1;
+        bool result = false;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                result = true;
+                break;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                // means the left part is sorted
+                if (nums[left] <= target && target <= nums[mid]) {
+                    // case 1: target on the left part
+                    right = mid - 1;
+                } else {
+                    // case 2: target on the right part
+                    left = mid + 1;
+                }
+            } else {
+                // means the left part is not sorted
+                // i.e. right part is sorted
+                if (nums[mid] <= target && target <= nums[right]) {
+                    // case 1: target on the right part
+                    left = mid + 1;
+                } else {
+                    // case 2: target on the left part
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return result;
     }
 };
 // @lc code=end
-
